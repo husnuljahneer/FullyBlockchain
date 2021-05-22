e7cd9cbfd5d97a14d79aa32d1db53cf98061defe
Husnul Jahneer <54552763+husnuljahneer@users.noreply.github.com>
2021-05-23 00:23:16 +0530

    BlockChain Chain Replacement

--- blockchain.js
+++ blockchain.js
@@ -10,8 +10,19 @@ class Blockchain {
             lastBlock:this.chain[this.chain.length-1],
             data
         });
-        this.chain.push(newBlock);
+        this.chain.push(newBlock);  
     }
+
+    replaceChain(chain){
+        if(chain.length <= this.chain.length){
+            return;
+        }
+        if(!Blockchain.isValidChain(chain)){
+            return;
+        }
+        this.chain = chain;
+    }
+
     static isValidChain(chain){
         if (JSON.stringify(chain[0])!==JSON.stringify(Block.genesis())){
         return false;
