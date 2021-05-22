a8593b80fe74f20ab8f35d8e388997db0eb62a4e
Husnul Jahneer <54552763+husnuljahneer@users.noreply.github.com>
2021-05-23 05:13:35 +0530

    Improved POW

--- Block.test.js
+++ Block.test.js
@@ -69,6 +69,13 @@ describe('Block',()=>{
         .toEqual('0'.repeat(minedBlock.difficulty));
     });
 
+    it('adjusts the difficulty', ()=> {
+      const possibleResults = [lastBlock.difficulty+1, lastBlock.difficulty-1];
+
+      expect(possibleResults.includes(minedBlock.difficulty)).toBe(true);
+    });
+    
+
     });
 
     describe('adjustDifficulty', () => {
@@ -83,6 +90,13 @@ describe('Block',()=>{
             originalBlock: block, timestamp: block.timestamp + MINE_RATE + 100
         })).toEqual(block.difficulty-1);
        }); 
+
+       it('has a lower limit of 1', () =>{
+        block.difficulty = -1;
+        expect(Block.adjustDifficulty({ originalBlock: block })).toEqual(1);
+       }); 
+
+
     });
     
     
