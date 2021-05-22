3f885bb472e608647e0a1fd06dcb3c0263dbcec6
Husnul Jahneer <54552763+husnuljahneer@users.noreply.github.com>
2021-05-22 21:57:57 +0530

    Added Genesis Block

--- Block.js
+++ Block.js
@@ -1,3 +1,5 @@
+const {GENESIS_DATA} = require('./config');
+
 class Block{
     constructor({timestamp,lastHash,hash,data}){
         this.timestamp = timestamp;
@@ -5,8 +7,13 @@ class Block{
         this.hash = hash;
         this.data = data;
     }
+
+    static genesis(){
+        return new this(GENESIS_DATA);
+    }
 }
 
 
-const block1 = new Block({timestamp:'01/01/01', lastHash:'foo-lasthash',hash:'foo-hash',data:'foo-data'});
-console.log('block1',block1);
+// const block1 = new Block({timestamp:'01/01/01', lastHash:'foo-lasthash',hash:'foo-hash',data:'foo-data'});
+// console.log('block1',block1);
+module.exports = Block;
---
+++ Block.test.js
@@ -0,0 +1,31 @@
+const Block = require('./block');
+const {GENESIS_DATA} = require('./config');
+
+describe('Block',()=>{
+    const timestamp = 'a-date' ;
+    const lastHash = 'foo-hash' ;
+    const hash = 'bar-hash' ;
+    const data = ['blockchain,data']
+    const block = new Block({
+        timestamp,lastHash,hash, data
+    });
+
+    it('has a  timestamp,lasthash,hash and data property',()=>{
+        expect(block.timestamp).toEqual(timestamp);
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+    });
+
+    describe('genesis()', () => {
+       const genesisBlock = Block.genesis(); 
+       it('returns a Block instance',()=>{
+           expect(genesisBlock instanceof Block).toBe(true);
+       });
+        
+       it('returns a genesis data', ()=>{
+        expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+    });
+    
+});
