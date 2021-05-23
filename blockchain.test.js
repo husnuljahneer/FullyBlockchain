9958d9423cc98603126224bb1535e7a917d609ca
Husnul Jahneer <54552763+husnuljahneer@users.noreply.github.com>
2021-05-23 06:11:10 +0530

    Core Logic Blockchain Done

--- blockchain.test.js
+++ blockchain.test.js
@@ -1,5 +1,6 @@
 const Blockchain = require ('./blockchain');
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 describe('Blockchain', () => {
     let blockchain,newChain,originalChain;
@@ -51,6 +52,30 @@ describe('Blockchain', () => {
                 expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
             });
         });
+
+        describe('and the chain contains a block with a jumped difficulty', () => {
+            it('returns false',()=>{
+                const lastBlock =  blockchain.chain[blockchain.chain.length-1];
+
+                const lastHash = lastBlock.hash;
+
+                const timestamp = Date.now();
+                const nonce = 0;
+                const data = [];
+                const difficulty = lastBlock.difficulty - 3;
+
+                const hash = cryptoHash(timestamp,lastHash,difficulty,nonce,data);
+                const badBlock = new Block({
+                    timestamp, lastBlock, hash, nonce, difficulty, data
+                });
+
+                blockchain.chain.push(badBlock);
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
+        });
+
+         
+
         describe('and the chain does not contain any invalid blocks', () => {
             it('returns true',()=>{
                 expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
