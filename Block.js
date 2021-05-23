b978820edf0ead2cd1cada30a19aeb7e23c0b09d
Husnul Jahneer <54552763+husnuljahneer@users.noreply.github.com>
2021-05-23 05:59:10 +0530

    Superior POW

--- Block.js
+++ Block.js
@@ -1,5 +1,7 @@
+const hexToBinary = require('hex-to-binary');
 const {GENESIS_DATA , MINE_RATE} = require('./config');
 const cryptoHash = require('./crypto-hash');
+
 
 
 class Block{
@@ -27,7 +29,7 @@ class Block{
             timestamp = Date.now();
             difficulty = Block.adjustDifficulty({originalBlock:lastBlock,timestamp});
             hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-        }while(hash.substring(0, difficulty) !=='0'.repeat(difficulty));
+        }while(hexToBinary(hash).substring(0, difficulty) !=='0'.repeat(difficulty));
 
         return new this({timestamp,lastHash,data,difficulty,nonce,hash});
     }
