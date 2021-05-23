b9b64e1dc29af087e6641273a9196d8b0ae56544
Husnul Jahneer <54552763+husnuljahneer@users.noreply.github.com>
2021-05-23 20:16:59 +0530

    API and Network
    The Blockchain Bacckend, API
    PubSub using Redis

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
-const fullyHash = (data) => {
-    return data + '*';
+const bodyParser = require('body-parser');
+const express = require ('express');
+const request = require('request');
+const Blockchain = require('./blockchain');
+const PubSub = require('./pubsub');
+
+
+const app = express();
+const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const  ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+
+app.use(bodyParser.json());
+
+app.get('/api/blocks', (req, res) => {
+    res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req,res)=>{
+    const { data } = req.body;
+
+    blockchain.addBlock({data});
+
+    pubsub.broadcastChain();
+
+    res.redirect('/api/blocks');
+});
+
+const syncChains = () => {
+    request({ url : `${ROOT_NODE_ADDRESS}/api/blocks`},(error, response, body) => {
+        if(!error && response.statusCode === 200){
+            const rootChain = JSON.parse(body);
+
+            console.log('replace chain on a sync with', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+};
+
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true'){
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 
-class Block{
-    constuctor(data,hash,lastHash){
-        this.data = data;
-        this.hash = hash;
-        this.lastHash = lastHash;
-    }
-}
+const PORT = PEER_PORT || DEFAULT_PORT;
 
-class Blockchain{
-    constructor(){
-        const genesis = new Block('gen-data' , 'gen-hash' ,'gen-lastHash');
-        this.chain = [genesis];
+app.listen( PORT ,()=>{
+    console.log(`listen at localhost:${PORT}`);
+    if(PORT !== DEFAULT_PORT){
+        syncChains();
     }
-    addBlock (data){
-        const lastHash = this.chain[this.chain.length-1].hash;
-        const hash = fullyHash(data + lastHash );
-        const block = new Block (data, hash, lastHash);
-        this.chain.push(block);
-    }
-}
+});
 
-const fooBlockchain = new Blockchain();
-fooBlockchain.addBlock('one');
-fooBlockchain.addBlock('two');
-fooBlockchain.addBlock('three');
-
-console.log(fooBlockchain);
