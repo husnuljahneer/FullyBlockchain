fd862624c61db9aeb462b92c4662de49c4645126
Husnul Jahneer <54552763+husnuljahneer@users.noreply.github.com>
2021-05-24 03:05:47 +0530

    Validated Transactions

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -59,4 +59,38 @@ describe('Transaction', () => {
        });
 
    });
+
+   describe('validTransaction', () => {
+       let errorMock;
+
+       beforeEach(() => {
+           errorMock = jest.fn();
+           global.console.error = errorMock;
+       });
+       describe('when the transaction is valid', () => {
+           it('returns true', () => {
+               expect(Transaction.validTransaction(transaction)).toBe(true);
+           });
+       });
+
+       describe('when the transaction is invalid', () => {
+           describe('and a transaction outputMap value in invalid', () => {
+            it('returns false and logs an error', () => {
+
+                transaction.outputMap[senderWallet.publicKey] = 999999;
+
+                expect(Transaction.validTransaction(transaction)).toBe(false);
+                expect(errorMock).toHaveBeenCalled();
+            });
+           });
+           describe('and the transaction input signature is invalid', () => {
+            it('returns false and logs an error', () => {
+                transaction.input.signature = new Wallet().sign('data');
+                expect(Transaction.validTransaction(transaction)).toBe(false);
+                expect(errorMock).toHaveBeenCalled();
+            });
+           });
+           
+       });
+   });
 });
